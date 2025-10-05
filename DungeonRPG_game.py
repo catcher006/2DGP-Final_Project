52c08a9decda4467c9faeabc67c2332bf796cf53
catcher006 <seungho217@tukorea.ac.kr>
2025-10-05 21:31:05 +0900

    village 배경 적용

--- DungeonRPG_game.py
+++ DungeonRPG_game.py
@@ -20,7 +20,23 @@ class Goblin_Boss:
     pass
 
 class Background_Resource:
-    pass
+    def __init__(self):
+        self.backgrounds = {
+            'village': load_image('village.png'),
+        }
+
+        # 현재 장소
+        self.current_location = 'village'
+
+
+    def update(self):
+        # 배경 업데이트 로직 (현재는 빈 메서드)
+        pass
+
+    def draw(self):
+        if self.current_location in self.backgrounds:
+            self.backgrounds[self.current_location].draw_to_origin(0, 0, 1024, 576)
+
 
 class Item:
     pass
@@ -34,17 +50,31 @@ def handle_events():
     pass
 
 def reset_world():
-    pass
+    global running
+    global world
+    global background
+
+    world = []
+    running = True
+
+    background = Background_Resource()
+    world.append(background)
 
 def update_world():
-    pass
+    for game_object in world:
+        game_object.update()
 
 def render_world():
-    pass
+    clear_canvas()
+
+    for game_object in world:
+        game_object.draw()
+
+    update_canvas()
 
 reset_world()
 
-while True:
+while running:
     handle_events()
     update_world()
     render_world()
