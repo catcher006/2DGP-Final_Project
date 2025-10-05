d55bac3cf82bae04be4a2d5f9c61b96b3d143386
catcher006 <seungho217@tukorea.ac.kr>
2025-10-05 14:42:56 +0900

    큰 기본 구조 정리

---
+++ DungeonRPG_game.py
@@ -0,0 +1,53 @@
+from pico2d import *
+import random, time
+
+class Slime_Mob:
+    pass
+
+class Slime_Boss:
+    pass
+
+class Skeleton_Mob:
+    pass
+
+class Skeleton_Boss:
+    pass
+
+class Goblin_Mob:
+    pass
+
+class Goblin_Boss:
+    pass
+
+class Background_Resource:
+    pass
+
+class Item:
+    pass
+
+class UI:
+    pass
+
+open_canvas(1024,1024)
+
+def handle_events():
+    pass
+
+def reset_world():
+    pass
+
+def update_world():
+    pass
+
+def render_world():
+    pass
+
+reset_world()
+
+while True:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.07)
+
+close_canvas()
