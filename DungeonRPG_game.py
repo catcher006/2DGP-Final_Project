40af658448ba6c1826b6944c8a98ca9ee31eb188
catcher006 <seungho217@tukorea.ac.kr>
2025-10-16 21:35:09 +0900

    활동별 Class 분리 작업 - 11

--- DungeonRPG_game.py
+++ DungeonRPG_game.py
@@ -1,6 +1,7 @@
 from pico2d import *
 
 from BackGround import Background
+from Back_Object import Back_Object
 from Front_object import Front_Object
 from Player import Player
 
@@ -23,23 +24,6 @@ class Goblin_Mob:
 class Goblin_Boss:
     pass
 
-
-class Back_Object:
-    def __init__(self):
-        self.back_objects = {
-            'village': load_image('village_home_door.png')  # 집 문 오브젝트 이미지
-        }
-        # 현재 장소
-        self.current_location = 'village'
-
-
-    def update(self):
-        # 배경 업데이트 로직 (현재는 빈 메서드)
-        pass
-
-    def draw(self):
-        if self.current_location in self.back_objects:
-            self.back_objects[self.current_location].clip_draw(0 * 1024, 1024 * 0, 1024, 1024, 182, 262, 100, 80)
 
 class Item:
     pass
