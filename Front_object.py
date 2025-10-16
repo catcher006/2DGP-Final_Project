786ff0c9a88e4d72b6a7d279f776267ebee8e69d
catcher006 <seungho217@tukorea.ac.kr>
2025-10-16 21:34:02 +0900

    활동별 Class 분리 작업 - 10

--- Front_object.py
+++ Front_object.py
@@ -1,20 +1,34 @@
 from pico2d import load_image
+from state_machine import StateMachine
 
+class VillageObject:
+    def __init__(self, fo):
+        self.fo = fo
+        self.image = load_image('village_objects.png')
+    def enter(self, e): pass
+    def exit(self, e): pass
+    def do(self): pass
+    def draw(self):
+        self.image.draw_to_origin(0, 0, 1024, 576)
+
+def to_village(e): return e == 'village'
 
 class Front_Object:
     def __init__(self):
-        self.front_objects = {
-            'village': load_image('village_objects.png')  # 표지판 이미지
-        }
+        self.VILLAGE = VillageObject(self)
 
-        # 현재 장소
-        self.current_location = 'village'
+        self.state_machine = StateMachine(
+            self.VILLAGE,
+            {
+                self.VILLAGE: {}
+            }
+        )
 
+    def set_location(self, location):
+        self.state_machine.handle_state_event(location)
 
     def update(self):
-        # 배경 업데이트 로직 (현재는 빈 메서드)
-        pass
+        self.state_machine.update()
 
     def draw(self):
-        if self.current_location in self.front_objects:
-            self.front_objects[self.current_location].draw_to_origin(0, 0, 1024, 576)
+        self.state_machine.draw()
