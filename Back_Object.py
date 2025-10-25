14a47806feb8e71e9495c924d0f9a3f5049e443d
catcher006 <seungho217@tukorea.ac.kr>
2025-10-25 23:25:02 +0900

    던전 입구 문 오브젝트 추가

--- Back_Object.py
+++ Back_Object.py
@@ -4,12 +4,12 @@ from state_machine import StateMachine
 class VillageBackObject:
     def __init__(self, bo):
         self.bo = bo
-        self.image = load_image('village_home_door.png')  # 집 문 오브젝트 이미지
+        self.image = load_image('deongeon_door.png')  # 집 문 오브젝트 이미지
     def enter(self, e): pass
     def exit(self, e): pass
     def do(self): pass
     def draw(self):
-        self.image.clip_draw(0, 0, 1024, 1024, 182, 262, 100, 80)
+        self.image.clip_draw(0, 0, 122, 104, 529, 440)
 
 def to_village(e): return e == 'village'
 
