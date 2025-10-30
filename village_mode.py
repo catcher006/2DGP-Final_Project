3be61d49082401b230a3e8f2434ddf27c21043f5
catcher006 <seungho217@tukorea.ac.kr>
2025-10-31 00:03:27 +0900

    game loop 부분 제거

--- village_mode.py
+++ village_mode.py
@@ -54,16 +54,3 @@ def pause():
 
 def resume():
     pass
-
-running = True
-
-open_canvas(1024,576)
-reset_world()
-
-while running:
-    handle_events()
-    update_world()
-    render_world()
-    delay(0.07)
-
-close_canvas()
