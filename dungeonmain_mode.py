ac1ab65d0fe74299f26d19e0efe0ea0eadfbeb9c
catcher006 <seungho217@tukorea.ac.kr>
2025-10-31 11:32:07 +0900

    init commit, 던전 메인 모드 파일 생성

--- dungeonmain_mode.py
+++ dungeonmain_mode.py
@@ -1,11 +1,32 @@
-from pico2d import load_image
+import game_framework
+from pico2d import *
 
-class Village:
-    def __init__(self):
-        self.image = load_image('dungeon_main.png')
-    def enter(self, e): pass
-    def exit(self, e): pass
-    def do(self): pass
-    def draw(self):
-        self.image.draw_to_origin(0, 0, 1024, 576)
-    def update(self): pass
+image = None
+
+def init():
+    global image
+    image = load_image('dungeon_main.png')
+
+def finish():
+    global image
+    del image
+
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+    image.draw_to_origin(0, 0, 1024, 576)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+
+def pause():
+    pass
+
+def resume():
+    pass
