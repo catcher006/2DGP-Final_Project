c5966ddab6d839d2194045392e23b43c1cf1dfe8
catcher006 <seungho217@tukorea.ac.kr>
2025-10-31 12:29:07 +0900

    village_mode.py 형식으로 변경

--- dungeonmain_mode.py
+++ dungeonmain_mode.py
@@ -1,29 +1,57 @@
-import game_framework
 from pico2d import *
 
-image = None
+import village_mode
+import game_world
+import game_framework
+from dungeonmain import Dungeonmain
+from Back_Object import Back_Object
+from Front_object import Front_Object
+from Player import Player
+
+
+def handle_events():
+    global running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_f):
+            game_framework.change_mode(village_mode)
+        else:
+            player.handle_event(event)
 
 def init():
-    global image
-    image = load_image('dungeon_main.png')
+    global world
+    global dungeonmain
+    global player
+    global back_object
+    global front_object
 
-def finish():
-    global image
-    del image
+    dungeonmain = Dungeonmain()
+    game_world.add_object((dungeonmain), 0)
+
+    # back_object = Back_Object()
+    # game_world.add_object((back_object), 1)
+
+    player = Player()
+    # 던전 메인 모드에서는 던전 메인 경로만 허용
+    player.allowed_paths = player.village_paths
+    game_world.add_object((player), 2)
+
+    # front_object = Front_Object()
+    # game_world.add_object((front_object), 3)
 
 def update():
-    pass
+    game_world.update()
 
 def draw():
     clear_canvas()
-    image.draw_to_origin(0, 0, 1024, 576)
+    game_world.render()
     update_canvas()
 
-def handle_events():
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            game_framework.quit()
+def finish():
+    game_world.clear()
 
 def pause():
     pass
