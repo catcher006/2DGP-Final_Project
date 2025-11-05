a2b0fb0295a68d2d1178d909166a8895b26bcb98
catcher006 <seungho217@tukorea.ac.kr>
2025-11-05 23:16:58 +0900

    던전 메인 이동 가능 구역 설정 - 추후 매끄럽게 적용 예정, 지금은 큭 ㄴ틀에서만 이동 가능하게 설정함

--- Player.py
+++ Player.py
@@ -184,7 +184,12 @@ class Player:
         self.dungeon_main_paths = [
             {'type': 'rect', 'min_x': 70, 'max_x': 895, 'min_y': 60, 'max_y': 220},  # 기본 아래 구역1
             {'type': 'rect', 'min_x': 895, 'max_x': 1000, 'min_y': 60, 'max_y': 210},  # 기본 아래 구역2
+            {'type': 'rect', 'min_x': 115, 'max_x': 880, 'min_y': 200, 'max_y': 280},  # 좌측 돌부리 - 우측 용암
             {'type': 'rect', 'min_x': 115, 'max_x': 290, 'min_y': 200, 'max_y': 310}, # 좌측 돌부리 - 중앙 나무 판자
+            {'type': 'rect', 'min_x': 380, 'max_x': 930, 'min_y': 280, 'max_y': 330},  # 좌측 돌부리 - 우측 용암
+            {'type': 'rect', 'min_x': 195, 'max_x': 290, 'min_y': 310, 'max_y': 400},  # 1번 문
+            {'type': 'rect', 'min_x': 505, 'max_x': 585, 'min_y': 330, 'max_y': 400},  # 2번 문
+            {'type': 'rect', 'min_x': 780, 'max_x': 880, 'min_y': 330, 'max_y': 400},  # 2번 문
             ]
 
         self.dungeon_inside_paths = []
