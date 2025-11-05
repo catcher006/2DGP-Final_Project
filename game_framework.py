d2899b9aa8b873c6303da0072ed8cc2cad463041
catcher006 <seungho217@tukorea.ac.kr>
2025-11-05 22:20:51 +0900

    프레임 타임 출력 주석화

--- game_framework.py
+++ game_framework.py
@@ -60,7 +60,7 @@ def run(start_mode):
         frame_time = time.time() - current_time
         frame_rate = 1.0 / frame_time
         current_time = time.time()
-        print(f'Frame Time: {frame_time:.4f}, Frame Rate: {frame_rate:.2f} FPS')
+        # print(f'Frame Time: {frame_time:.4f}, Frame Rate: {frame_rate:.2f} FPS')
 
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
