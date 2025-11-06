8969ea44362ecb826b78ff0042891ad01f37fae8
catcher006 <seungho217@tukorea.ac.kr>
2025-11-06 15:03:20 +0900

    최대 방 번호 설정

--- stage1.py
+++ stage1.py
@@ -3,6 +3,7 @@ from pico2d import load_image
 class Stage1:
     def __init__(self):
         self.room_number = 0
+        self.max_room_number = 7
         self.image = load_image("./image/background/stage1/%d.png" % self.room_number)
     def enter(self, e): pass
     def exit(self, e): pass
