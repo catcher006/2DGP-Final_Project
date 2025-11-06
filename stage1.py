03f78172396df23297cdaa3218b949c0d1bf31d8
catcher006 <seungho217@tukorea.ac.kr>
2025-11-06 14:22:24 +0900

    stage1 백그라운드 파일 불러오기 py파일 생성

---
+++ stage1.py
@@ -0,0 +1,11 @@
+from pico2d import load_image
+
+class Stage1:
+    def __init__(self):
+        self.image = load_image('./image/background/stage1/0.png')
+    def enter(self, e): pass
+    def exit(self, e): pass
+    def do(self): pass
+    def draw(self):
+        self.image.draw_to_origin(0, 0, 1024, 576)
+    def update(self): pass
