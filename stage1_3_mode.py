35fe85bfc8a11a052169c268cb5eb2edb2fc9f32
catcher006 <seungho217@tukorea.ac.kr>
2025-11-07 21:13:05 +0900

    stage3 <-> stage0 이동 구현

--- stage1_3_mode.py
+++ stage1_3_mode.py
@@ -4,8 +4,8 @@ import random
 import dungeonmain_mode
 import game_world
 import game_framework
-import stage1_1_mode
-from stage1_2 import Stage1_2
+import stage1_0_mode
+from stage1_3 import Stage1_3
 from player import Player
 from slime_mob import Slime_Mob
 
@@ -19,31 +19,31 @@ def handle_events():
             game_framework.quit()
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_f):
             if 500 <= player.x <=  550 and 580 <= player.y <= 600: # 상단 문
-                game_framework.change_mode(dungeonmain_mode)
+                game_framework.change_mode(stage1_0_mode)
             elif 990 <= player.x <=  1100 and 270 <= player.y <= 370: # 우측 문
                 game_framework.change_mode(dungeonmain_mode)
             elif 500 <= player.x <=  550 and 0 <= player.y <= 20: # 하단 문
                 game_framework.change_mode(dungeonmain_mode)
             elif 0 <= player.x <=  20 and 270 <= player.y <= 370: # 좌측 문
-                game_framework.change_mode(stage1_1_mode)
+                game_framework.change_mode(dungeonmain_mode)
         else:
             player.handle_event(event)
 
 def init():
     global world
-    global stage1_2
+    global stage1_3
     global player
     global back_object
     global front_object
 
-    stage1_2 = Stage1_2()
-    game_world.add_object(stage1_2, 0)
+    stage1_3 = Stage1_3()
+    game_world.add_object(stage1_3, 0)
 
     # back_object = Back_Object()
     # game_world.add_object((back_object), 1)
 
     player = Player()
-    player.move_validator = stage1_2.is_walkable
+    player.move_validator = stage1_3.is_walkable
     player.x = 535
     player.y = 540
 
@@ -51,7 +51,7 @@ def init():
 
     slime_mobs = [Slime_Mob() for _ in range(random.randint(0, 2))]
     for slime_mob in slime_mobs:
-        slime_mob.move_validator = stage1_2.is_mob_walkable
+        slime_mob.move_validator = stage1_3.is_mob_walkable
     game_world.add_objects(slime_mobs, 2)
 
 
