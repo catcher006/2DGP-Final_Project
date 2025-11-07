0803ebad9abba963558aff34271c7417acd156c8
catcher006 <seungho217@tukorea.ac.kr>
2025-11-07 21:38:03 +0900

    stage7 <-> stage4, stage7 <-> stage6

--- stage1_6_mode.py
+++ stage1_6_mode.py
@@ -4,7 +4,7 @@ import random
 import dungeonmain_mode
 import game_world
 import game_framework
-import stage1_1_mode
+import stage1_7_mode
 import stage1_3_mode
 from stage1_6 import Stage1_6
 from player import Player
@@ -22,7 +22,7 @@ def handle_events():
             if 500 <= player.x <=  550 and 580 <= player.y <= 600: # 상단 문
                 game_framework.change_mode(stage1_3_mode)
             elif 990 <= player.x <=  1100 and 270 <= player.y <= 370: # 우측 문
-                game_framework.change_mode(dungeonmain_mode)
+                game_framework.change_mode(stage1_7_mode)
             elif 500 <= player.x <=  550 and 0 <= player.y <= 20: # 하단 문
                 game_framework.change_mode(dungeonmain_mode)
             elif 0 <= player.x <=  20 and 270 <= player.y <= 370: # 좌측 문
--- stage1_7_mode.py
+++ stage1_7_mode.py
@@ -4,9 +4,9 @@ import random
 import dungeonmain_mode
 import game_world
 import game_framework
-import stage1_1_mode
-import stage1_3_mode
-from stage1_0 import Stage1_0
+import stage1_4_mode
+import stage1_6_mode
+from stage1_7 import Stage1_7
 from player import Player
 from slime_mob import Slime_Mob
 
@@ -20,31 +20,31 @@ def handle_events():
             game_framework.quit()
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_f):
             if 500 <= player.x <=  550 and 580 <= player.y <= 600: # 상단 문
-                game_framework.change_mode(dungeonmain_mode)
+                game_framework.change_mode(stage1_4_mode)
             elif 990 <= player.x <=  1100 and 270 <= player.y <= 370: # 우측 문
-                game_framework.change_mode(stage1_1_mode)
-            elif 500 <= player.x <=  550 and 0 <= player.y <= 20: # 하단 문
-                game_framework.change_mode(stage1_3_mode)
-            elif 0 <= player.x <=  20 and 270 <= player.y <= 370: # 좌측 문
                 game_framework.change_mode(dungeonmain_mode)
+            elif 500 <= player.x <=  550 and 0 <= player.y <= 20: # 하단 문
+                game_framework.change_mode(dungeonmain_mode)
+            elif 0 <= player.x <=  20 and 270 <= player.y <= 370: # 좌측 문
+                game_framework.change_mode(stage1_6_mode)
         else:
             player.handle_event(event)
 
 def init():
     global world
-    global stage1_0
+    global stage1_7
     global player
     global back_object
     global front_object
 
-    stage1_0 = Stage1_0()
-    game_world.add_object(stage1_0, 0)
+    stage1_7 = Stage1_7()
+    game_world.add_object(stage1_7, 0)
 
     # back_object = Back_Object()
     # game_world.add_object((back_object), 1)
 
     player = Player()
-    player.move_validator = stage1_0.is_walkable
+    player.move_validator = stage1_7.is_walkable
     player.x = 535
     player.y = 540
 
@@ -52,7 +52,7 @@ def init():
 
     slime_mobs = [Slime_Mob() for _ in range(random.randint(0, 2))]
     for slime_mob in slime_mobs:
-        slime_mob.move_validator = stage1_0.is_mob_walkable
+        slime_mob.move_validator = stage1_7.is_mob_walkable
     game_world.add_objects(slime_mobs, 2)
 
 
