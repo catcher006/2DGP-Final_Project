6425bc82e6d93c16522fbb17d02b13da1de47e21
catcher006 <seungho217@tukorea.ac.kr>
2025-11-16 15:07:14 +0900

    각 방 생성 여부, 마지막 player 좌표, 마지막 몹 좌표 전역 변수 관리

---
+++ stage1_manger.py
@@ -0,0 +1,37 @@
+# 각 방 생성 여부 전역 변수
+stage1_0_create = None
+stage1_1_create = None
+stage1_2_create = None
+stage1_3_create = None
+stage1_4_create = None
+stage1_5_create = None
+stage1_6_create = None
+stage1_7_create = None
+
+# 각 방 마지막 player 좌표 전역 변수
+stage1_0_last_player_pos = None
+stage1_1_last_player_pos = None
+stage1_2_last_player_pos = None
+stage1_3_last_player_pos = None
+stage1_4_last_player_pos = None
+stage1_5_last_player_pos = None
+stage1_6_last_player_pos = None
+stage1_7_last_player_pos = None
+
+# 각 방 마지막 몹 좌표 전역 변수
+stage1_0_last_mob1_pos = None
+stage1_0_last_mob2_pos = None
+stage1_1_last_mob1_pos = None
+stage1_1_last_mob2_pos = None
+stage1_2_last_mob1_pos = None
+stage1_2_last_mob2_pos = None
+stage1_3_last_mob1_pos = None
+stage1_3_last_mob2_pos = None
+stage1_4_last_mob1_pos = None
+stage1_4_last_mob2_pos = None
+stage1_5_last_mob1_pos = None
+stage1_5_last_mob2_pos = None
+stage1_6_last_mob1_pos = None
+stage1_6_last_mob2_pos = None
+stage1_7_last_mob1_pos = None
+stage1_7_last_mob2_pos = None
