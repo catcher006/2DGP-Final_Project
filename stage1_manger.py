e823d8f928028304895a07bb02c5f7351303b16f
catcher006 <seungho217@tukorea.ac.kr>
2025-11-18 16:37:33 +0900

    객체 지향 프로그래밍을 위한 전역 변수 일부 제거

--- stage1_manger.py
+++ stage1_manger.py
@@ -11,50 +11,10 @@ stage1_5_create = None
 stage1_6_create = None
 stage1_7_create = None
 
-# 각 방 마지막 몹 상태 전역 변수 (리스트 형태로 몹의 dict 저장)
-stage1_0_mobs = None
-stage1_1_mobs = None
-stage1_2_mobs = None
-stage1_3_mobs = None
-stage1_4_mobs = None
-stage1_5_mobs = None
-stage1_6_mobs = None
-stage1_7_mobs = None
-
-# 각 방 마지막 몹 좌표 전역 변수
-stage1_0_last_mob1_pos = None
-stage1_0_last_mob2_pos = None
-stage1_1_last_mob1_pos = None
-stage1_1_last_mob2_pos = None
-stage1_2_last_mob1_pos = None
-stage1_2_last_mob2_pos = None
-stage1_3_last_mob1_pos = None
-stage1_3_last_mob2_pos = None
-stage1_4_last_mob1_pos = None
-stage1_4_last_mob2_pos = None
-stage1_5_last_mob1_pos = None
-stage1_5_last_mob2_pos = None
-stage1_6_last_mob1_pos = None
-stage1_6_last_mob2_pos = None
-stage1_7_last_mob1_pos = None
-stage1_7_last_mob2_pos = None
 
 def reset_stage1_manager():
     global stage1_0_create, stage1_1_create, stage1_2_create, stage1_3_create
     global stage1_4_create, stage1_5_create, stage1_6_create, stage1_7_create
-    global stage1_0_last_player_pos, stage1_1_last_player_pos, stage1_2_last_player_pos
-    global stage1_3_last_player_pos, stage1_4_last_player_pos, stage1_5_last_player_pos
-    global stage1_6_last_player_pos, stage1_7_last_player_pos
-    global stage1_0_last_mob1_pos, stage1_0_last_mob2_pos
-    global stage1_1_last_mob1_pos, stage1_1_last_mob2_pos
-    global stage1_2_last_mob1_pos, stage1_2_last_mob2_pos
-    global stage1_3_last_mob1_pos, stage1_3_last_mob2_pos
-    global stage1_4_last_mob1_pos, stage1_4_last_mob2_pos
-    global stage1_5_last_mob1_pos, stage1_5_last_mob2_pos
-    global stage1_6_last_mob1_pos, stage1_6_last_mob2_pos
-    global stage1_7_last_mob1_pos, stage1_7_last_mob2_pos
-    global stage1_0_mobs, stage1_1_mobs, stage1_2_mobs, stage1_3_mobs
-    global stage1_4_mobs, stage1_5_mobs, stage1_6_mobs, stage1_7_mobs
     global stage1_in_game
 
     stage1_in_game = False
@@ -67,54 +27,3 @@ def reset_stage1_manager():
     stage1_5_create = None
     stage1_6_create = None
     stage1_7_create = None
-
-    stage1_0_last_mob1_pos = None
-    stage1_0_last_mob2_pos = None
-    stage1_1_last_mob1_pos = None
-    stage1_1_last_mob2_pos = None
-    stage1_2_last_mob1_pos = None
-    stage1_2_last_mob2_pos = None
-    stage1_3_last_mob1_pos = None
-    stage1_3_last_mob2_pos = None
-    stage1_4_last_mob1_pos = None
-    stage1_4_last_mob2_pos = None
-    stage1_5_last_mob1_pos = None
-    stage1_5_last_mob2_pos = None
-    stage1_6_last_mob1_pos = None
-    stage1_6_last_mob2_pos = None
-    stage1_7_last_mob1_pos = None
-    stage1_7_last_mob2_pos = None
-
-    stage1_0_mobs = None
-    stage1_1_mobs = None
-    stage1_2_mobs = None
-    stage1_3_mobs = None
-    stage1_4_mobs = None
-    stage1_5_mobs = None
-    stage1_6_mobs = None
-    stage1_7_mobs = None
-
-
-def stage1_0_slime_mobs():
-    return stage1_0_mobs
-
-def stage1_1_slime_mobs():
-    return stage1_1_mobs
-
-def stage1_2_slime_mobs():
-    return stage1_2_mobs
-
-def stage1_3_slime_mobs():
-    return stage1_3_mobs
-
-def stage1_4_slime_mobs():
-    return stage1_4_mobs
-
-def stage1_5_slime_mobs():
-    return stage1_5_mobs
-
-def stage1_6_slime_mobs():
-    return stage1_6_mobs
-
-def stage1_7_slime_mobs():
-    return stage1_7_mobs
