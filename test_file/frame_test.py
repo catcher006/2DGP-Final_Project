eb8f7e01e1ad8e78dbd27c3eba3f5b796d13cc96
catcher006 <seungho217@tukorea.ac.kr>
2025-11-28 20:30:44 +0900

    좀비 보스 몹 몸체 충동 박스 구역 테스트

--- test_file/frame_test.py
+++ test_file/frame_test.py
@@ -19,16 +19,16 @@ class Attack:
         pass
 
     def draw(self):
-        self.mob.attack_image.clip_draw(int(self.mob.frame) * 192, 192 * self.mob.face_dir, 192, 192, self.mob.x, self.mob.y, 300, 300)
+        self.mob.move_image.clip_draw(int(self.mob.frame) * 64, 64 * self.mob.face_dir, 64, 64, self.mob.x, self.mob.y, 200, 200)
 
 class Slime_Mob:
     def __init__(self):
-        self.attack_image = load_image("../image/mobs/zombie/mace/attack.png")
+        self.move_image = load_image("../image/mobs/zombie_boss/idle.png")
 
         self.x = 600
         self.y = 400
 
-        self.frame = 3
+        self.frame = 0
         self.face_dir = 3
 
         self.dx = 0.0
@@ -68,36 +68,14 @@ class Slime_Mob:
 
     def get_bb(self):
         # face_dir = 0
-        # return self.x + 22, self.y - 42, self.x + 40, self.y - 26  # frame : 0
-        # return self.x + 4, self.y - 32, self.x + 22, self.y - 14  # frame : 1
-        # return self.x - 20, self.y - 32, self.x - 2, self.y - 14  # frame : 2
-        # return self.x + 8, self.y - 26, self.x + 32, self.y - 8  # frame : 3
-        # return self.x + 36, self.y - 10, self.x + 70, self.y + 12  # frame : 4
-        # return self.x + 34, self.y - 4, self.x + 52, self.y + 12  # frame : 5
-
+        # return self.x - 34, self.y - 94, self.x + 28, self.y + 52
         # face_dir = 1
-        # return self.x - 10, self.y - 44, self.x + 10, self.y - 28  # frame : 0
-        # return self.x - 26, self.y - 30, self.x - 10, self.y - 14  # frame : 1
-        # return self.x - 44, self.y - 24, self.x - 26, self.y - 6  # frame : 2
-        # return self.x - 26, self.y - 40, self.x - 8, self.y - 24  # frame : 3
-        # return self.x + 26, self.y - 50, self.x + 58, self.y - 24  # frame : 4
-        # return self.x + 54, self.y - 28, self.x + 72, self.y - 12  # frame : 5
-
+        # return self.x - 48, self.y - 94, self.x + 46, self.y + 52
         # face_dir = 2
-        # return self.x - 40, self.y - 42, self.x - 22, self.y - 26  # frame : 0
-        # return self.x - 24, self.y - 32, self.x - 6, self.y - 14  # frame : 1
-        # return self.x, self.y - 32, self.x + 18, self.y - 14  # frame : 2
-        # return self.x - 34, self.y - 26, self.x - 10, self.y - 8  # frame : 3
-        # return self.x - 70, self.y - 10, self.x - 36, self.y + 14  # frame : 4
-        # return self.x - 52, self.y - 4, self.x - 34, self.y + 14  # frame : 5
-
+        # return self.x - 28, self.y - 94, self.x + 34, self.y + 52
         # face_dir = 3
-        # return self.x - 10, self.y - 44, self.x + 10, self.y - 28  # frame : 0
-        # return self.x - 14, self.y - 32, self.x + 5, self.y - 15  # frame : 1
-        # return self.x - 38, self.y - 20, self.x - 20, self.y - 2  # frame : 2
-        # return self.x - 18, self.y - 8, self.x + 9, self.y + 16  # frame : 3
-        # return self.x - 10, self.y + 4, self.x + 58, self.y + 34  # frame : 4
-        # return self.x + 38, self.y - 14, self.x + 66, self.y + 4  # frame : 5
+        return self.x - 48, self.y - 94, self.x + 46, self.y + 50
+
 
 
 
