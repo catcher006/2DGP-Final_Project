69594437f803d7db714f067d171b76ebf1921099
catcher006 <seungho217@tukorea.ac.kr>
2025-12-02 15:41:48 +0900

    코인 UI 추가 및 UI 일부 개선

--- ui.py
+++ ui.py
@@ -5,21 +5,15 @@ class Ui:
     coin = 100
     hp = 100
     def __init__(self):
-        self.coin_font = load_font('ENCR10B.TTF', 25)
-        self.hp_font = load_font('ENCR10B.TTF', 15)
+        self.coin_font = load_font('./font/PixelPurl.TTF', 25)
+        self.hp_font = load_font('./font/PixelPurl.TTF', 20)
         self.hp_image = load_image('./image/ui/player/player_hp.png')
+        self.ui_coin_image = load_image('./image/ui/item/coin.png')
     def draw(self):
-        self.coin_font.draw(20, 560, f'Coin: {Ui.coin:06d}', (255, 255, 0))
+        self.ui_coin_image.draw(60, 560)
+        self.coin_font.draw(70, 560, f'{Ui.coin:d}', (255, 255, 0))
         self.hp_image.clip_draw(0, int(Ui.hp) * 54, 400, 54, 150, 540, 240, 15)
-        if Ui.hp >= 100:
-            self.hp_font.draw(140, 540, f'{Ui.hp:02d}', (255, 255, 255))
-        elif 50 < Ui.hp < 100:
-            self.hp_font.draw(150, 540, f'{Ui.hp:02d}', (255, 255, 255))
-        elif Ui.hp == 50:
-            self.hp_font.draw(143, 540, f'{((Ui.hp % 100) // 10):d}', (255, 255, 255))
-            self.hp_font.draw(150, 540, f'{Ui.hp % 10:d}', (255, 0, 255))
-        else:
-            self.hp_font.draw(150 - 6, 540, f'{Ui.hp:02d}', (255, 0, 255))
+        self.hp_font.draw(140, 560, f'Player HP: {Ui.hp:d}', (255, 255, 255))
     def update(self):
         Ui.hp = player.player_hp
     def enter(self, e): pass
